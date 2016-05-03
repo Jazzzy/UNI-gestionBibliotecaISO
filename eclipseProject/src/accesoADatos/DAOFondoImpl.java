2740bd6c81a16bac88de812423f31087aa05f4d4
Pablo Tomico Pérez <pablotomico@hotmail.com>
2016-05-03 22:19:56 +0200

    Implementado parte de DAOFondoImpl

--- eclipseProject/src/accesoADatos/DAOFondoImpl.java
+++ eclipseProject/src/accesoADatos/DAOFondoImpl.java
@@ -85,8 +85,38 @@ public class DAOFondoImpl implements DAOFondo {
 			ResultSet res = pstmt.executeQuery();
 			
 			ArrayList<VOFondo> fondos = new ArrayList<>();
-
+			VOFondo fondo; 
 			while (res.next()) {
+				fondo = new VOFondo(res.getInt("id"), res.getString("titulo"), res.getDate("fecha_compra"));
+				fondos.add(fondo);
+			}
+			return fondos;
+
+		} catch (
+
+		SQLException e) {
+			System.out.println("Error en la consulta");
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public VOFondo getFondoByISBN(String ISBN) {
+		try {
+			Connector conector = new Connector();
+			Connection con = conector.getConnection();
+
+			PreparedStatement pstmt = null;
+
+			String sqlSelect = "SELECT * FROM Fondo " + " WHERE isbn = ?;";
+
+			pstmt = con.prepareStatement(sqlSelect);
+			pstmt.setString(1, ISBN);
+
+			ResultSet res = pstmt.executeQuery();
+
+			if (res.next()) {
 				VOFondo fondo = new VOFondo(res.getInt("id"), res.getString("titulo"), res.getDate("fecha_compra"));
 				return fondo;
 			}
@@ -101,26 +131,111 @@ public class DAOFondoImpl implements DAOFondo {
 	}
 
 	@Override
-	public VOFondo getFondoByISBN(String ISBN) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public ArrayList<VOFondo> getFondos() {
-		// TODO Auto-generated method stub
+		try {
+			Connector conector = new Connector();
+			Connection con = conector.getConnection();
+
+			PreparedStatement pstmt = null;
+
+			String sqlSelect = "SELECT * FROM Fondo;";
+
+			pstmt = con.prepareStatement(sqlSelect);
+			
+			ResultSet res = pstmt.executeQuery();
+			
+			ArrayList<VOFondo> fondos = new ArrayList<>();
+			VOFondo fondo; 
+			while (res.next()) {
+				fondo = new VOFondo(res.getInt("id"), res.getString("titulo"), res.getDate("fecha_compra"));
+				fondos.add(fondo);
+			}
+			return fondos;
+
+		} catch (
+
+		SQLException e) {
+			System.out.println("Error en la consulta");
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public ArrayList<VOFondo> getFondosByAutor(String autor) {
-		// TODO Auto-generated method stub
+		try {
+			Connector conector = new Connector();
+			Connection con = conector.getConnection();
+
+			PreparedStatement pstmt = null;
+
+			String sqlSelect = "select * from (select f.*, l.ano_edicion, l.editorial, l.isbn, a.autor from "
+					+ "Fondo f inner join Libro l on f.id = l.id "
+					+ "inner join Autores a on a.id = l.id) "
+					+ "as aux where aux.autor=?;";
+
+			pstmt = con.prepareStatement(sqlSelect);
+			pstmt.setString(1, autor);
+
+			ResultSet res = pstmt.executeQuery();
+			
+			ArrayList<VOFondo> fondos = new ArrayList<>();
+			VOFondo fondo;
+			ArrayList<String> autores = new ArrayList<>();
+			autores.add(autor);
+			while (res.next()) {
+				fondo = new VOLibro(res.getInt("id"), res.getString("titulo"), 
+						res.getDate("fecha_compra"), res.getString("isbn"), 
+						autores, res.getDate("ano_edicion"), res.getString("editorial"));
+				fondos.add(fondo);
+			}
+			return fondos;
+
+		} catch (
+
+		SQLException e) {
+			System.out.println("Error en la consulta");
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public ArrayList<VOFondo> getFondosByAnoEdicion(Date desde, Date hasta) {
-		// TODO Auto-generated method stub
+		try {
+			Connector conector = new Connector();
+			Connection con = conector.getConnection();
+
+			PreparedStatement pstmt = null;
+
+			String sqlSelect = "SELECT * FROM (select f.*, l.ano_edicion, l.editorial, l.isbn from "
+					+ "Fondo f INNER JOIN Libro l ON f.id = l.id)"
+					+ "AS aux WHERE aux.ano_edicion between ? and ?";
+
+			pstmt = con.prepareStatement(sqlSelect);
+			pstmt.setDate(1, new java.sql.Date(desde.getTime()));
+			pstmt.setDate(2, new java.sql.Date(hasta.getTime()));
+
+			ResultSet res = pstmt.executeQuery();
+			
+			ArrayList<VOFondo> fondos = new ArrayList<>();
+			VOFondo fondo;
+			ArrayList<String> autores = new ArrayList<>();
+			//meter aqui autores
+			while (res.next()) {
+				fondo = new VOLibro(res.getInt("id"), res.getString("titulo"), 
+						res.getDate("fecha_compra"), res.getString("isbn"), 
+						autores, res.getDate("ano_edicion"), res.getString("editorial"));
+				fondos.add(fondo);
+			}
+			return fondos;
+
+		} catch (
+
+		SQLException e) {
+			System.out.println("Error en la consulta");
+			e.printStackTrace();
+		}
 		return null;
 	}
 
